5bd1312533ac8372733558a50a1ed2d9eb65a161
FlorentinTh <florentin.thullier1@uqac.ca>
2016-03-08 18:11:08 -0500

    Final fix.

--- app/src/main/java/ca/uqac/florentinth/speakerauthentication/Database/Controllers/UserLocationController.java
+++ app/src/main/java/ca/uqac/florentinth/speakerauthentication/Database/Controllers/UserLocationController.java
@@ -12,7 +12,19 @@ import ca.uqac.florentinth.speakerauthentication.Database.DatabaseHelper;
 import ca.uqac.florentinth.speakerauthentication.Database.Models.UserLocation;
 
 /**
- * Created by FlorentinTh on 1/23/2016.
+ * Copyright 2016 Florentin Thullier.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class UserLocationController {
 
